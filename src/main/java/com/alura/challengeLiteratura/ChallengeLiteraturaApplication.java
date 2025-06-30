57cb5026edcad33630eea3ae543dedc86e8d3516
Nicolas Barbetta <nbarbetta@gmail.com>
2025-06-30 10:49:19 -0300

    sincro java#2

--- src/main/java/com/alura/challengeLiteratura/ChallengeLiteraturaApplication.java
+++ src/main/java/com/alura/challengeLiteratura/ChallengeLiteraturaApplication.java
@@ -1,13 +1,24 @@
 package com.alura.challengeLiteratura;
 
+import com.alura.challengeLiteratura.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChallengeLiteraturaApplication {
+public class ChallengeLiteraturaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ChallengeLiteraturaApplication.class, args);
+	}
+	@Override
+	public void run(String... args) throws Exception {
+
+		Principal principal = new Principal();
+		principal.iniciarMenu();
+
+
 	}
 
 }
